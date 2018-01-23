6ef6324beb4f57ee1e9e8fa5cd9f637d31f0ce46
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-22 17:44:59 -0800

    sleepy head Simple calculator

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -6,20 +6,37 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+	String c=	JOptionPane.showInputDialog("Give me a number");
+	int a= Integer.parseInt(c);
+	String d=	JOptionPane.showInputDialog("Give me another number");
+	int b= Integer.parseInt(d);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+		int operation = JOptionPane.showOptionDialog(null, "Do you want to:", "Math Sutff", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "add", "subtract", "multiply", "divide" },
 				null);
-
+	
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
 
+		if(operation==0) {
+		add(a,b);	
+		}
+		
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
-	}
-
+	
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
+	}
+	
+	
+	static void add(int a, int b) {
+		JOptionPane.showMessageDialog(null, a+"+" +b+ "="+(a+b));}
+	
 
 	// 4. Create similar methods for subtraction, multiplication and division.
+	static void subtract(int a, int b) {
+		JOptionPane.showMessageDialog(null, a+"-" +b+ "=" +(a-b));
+
+	
+	}}
 }
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -13,16 +13,30 @@ public class SleepyHead {
         
         /*
          * Ask the user for these values using
+         *
          * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
          * JOptionPane.YES_NO_OPTION);
          */
-        
-
-
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
+       int x= JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+      System.out.println(x);
+       
+      if (x==0) {
+    	JOptionPane.showMessageDialog(null, "Get up lazy bones!!!");
+    	  }
+    if (x==1) {
+    	JOptionPane.showMessageDialog(null, "Sleep in");
+    }
+    
+    
+    
+    
+    }
+      
+      
+      /*
+         *          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
          */
     }
-}
+
